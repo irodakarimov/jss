b7058c8a7e70091b2eb4bcb632dd5c0b4af01d03
irodakarimov <irodakarimova896@gmail.com>
2024-11-06 15:59:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var a = 5
+let b = 853290
+const sux = 87
+
+let userName = prompt('Ismingizni kiriting');
+let surName = prompt('Familiyangizni kiriting');
+let ageName = prompt('Yoshingizni kiriting');
+let favoritesubject = prompt('Yoqtirgan faningiz yozing');
+let livingdistrict = prompt('Yashayotgan tumaningiz kiriting');
+alert(`Malumotlaringizni kiritganingiz uchun tashakkur ${userName}`);
+console.log(`Ism: Muhammad ayyub ${userName}`)
+console.log(`Familiya: Zafarov ${userName}`)
+console.log(`Yosh: 12 ${userName}`)
+console.log(`Yoqtirgan fan: Informatika ${userName}`)
+console.log(`Yashayotgan tuman: Yunusobod ${userName}`)
